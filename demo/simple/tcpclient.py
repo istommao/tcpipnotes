a3b19ce61eceb46c9715aeb4e4b1aeccc751001f
istommao <istommao@gmail.com>
2018-08-15 14:44:52 +0800

    Fix tcp http demo bug

--- demo/simple/tcpclient.py
+++ demo/simple/tcpclient.py
@@ -2,15 +2,16 @@
 import socket
 
 ipaddr = '127.0.0.1'
-port = 8080
+port = 8060
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((ipaddr, port))
 
-client.send('Hello'.encode('utf-8'))
+data = """GET / HTTP/1.0\r\n\r\nHello world!"""
+client.send(data.encode('utf-8'))
 
-resp = client.recvfrom(1024)
+resp = client.recvfrom(2048)
 print(resp[0].decode())
 
 client.close()
--- demo/simple/tcpserver.py
+++ demo/simple/tcpserver.py
@@ -4,12 +4,12 @@ import socket
 EOL1 = b'\n\n'
 EOL2 = b'\n\r\n'
 
-response = """HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght:13\r\n\r\nHello world!"""
+response = """HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n\r\nHello world!"""
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-port = 8080
+port = 8060
 
 server.bind(('127.0.0.1', port))
 
@@ -26,9 +26,10 @@ try:
 
         request = b''
         while EOL1 not in request and EOL2 not in request:
-            request += conn.recv(1024)
+            request += conn.recv(2048)
 
         print(request.decode())
+
         conn.send(response.encode())
         conn.close()
 except KeyboardInterrupt:
